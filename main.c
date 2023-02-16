d6a995326820de9d0d694cf9de7f9f8c76dc76b9
The Magppi <skiderdun@gmail.com>
2023-02-16 07:29:00 -0600

    sdl

---
+++ main.c
@@ -0,0 +1,20 @@
+
+// a small rouglike game
+// 1/15/2023
+// by: Tommy Weiss
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "SDL.h"
+
+// test area
+
+
+
+// end test area
+
+int main(void)
+
+{
+    return 1;
+}
